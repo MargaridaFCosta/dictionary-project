65cf1431488a4cbf22af70ed7f5a6b5ceb1a8527
MargaridaFCosta <margaridaxavicosta@hotmail.com>
2023-03-12 18:04:00 +0000

    Resolve the dictionary error

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,17 +3,16 @@ import axios from "axios";
 import Results from "./Results";
 
 export default function Dictionary() {
-  let [insertWord, setInsertWord] = useState("");
+  let [insertWord, setInsertWord] = useState(null);
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    //console.log(response.data)
     setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${insertWord}`);
     let apiKey = "ato2b04e4f46da013787d91355bf798f";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${insertWord}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
--- src/Meanings.js
+++ src/Meanings.js
@@ -1,19 +1,16 @@
 import React from "react";
 
 export default function Meanings(props) {
-    return (
-      <div className="Meanings">
-        <h3>{props.meanings.partOfSpeech}</h3>
-            {props.meanings.definition.map(function (definition, index) {
-                return (
-                    <div key={index}>
-                        <p>
-                            {definition.definition}
-                            {definition.example}
-                        </p>
-                    </div>
-                );
-            })}
-      </div>
-    );
+console.log(props.meanings);
+  return (
+    <div className="Meanings">
+      <h3>{props.meanings.partOfSpeech}</h3>
+      <h2>
+        <em>{props.meanings.definition}</em>
+      </h2>
+
+      <h2>{props.meanings.example}</h2>
+      <br />
+    </div>
+  );
 }
