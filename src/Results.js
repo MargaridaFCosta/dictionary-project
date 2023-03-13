1abd871dcb0f711f2d2c689ddba8b6c063dec277
MargaridaFCosta <margaridaxavicosta@hotmail.com>
2023-03-13 21:03:43 +0000

    Added the phonetics

--- src/Results.js
+++ src/Results.js
@@ -5,17 +5,19 @@ import "./Results.css";
 export default function Results(props) {
     if (props.results) {
         return (
-        <div className="Results p-4">
-          <h2 className="name-word mb-4">{props.results.word}</h2>
+          <div className="Results p-4">
+            <h2 className="name-word mb-3">{props.results.word}</h2>
+            <h3 className="phonetics">{props.results.phonetic}</h3>
+            <br />
             {props.results.meanings.map(function (meanings, index) {
-            return (
-              <div key={index}>
-                <Meanings meanings={meanings} />
-              </div>
-            );
-          })}
-        </div>
-      );
+              return (
+                <div key={index}>
+                  <Meanings meanings={meanings} />
+                </div>
+              );
+            })}
+          </div>
+        );
     } else {
         return null;
     }
